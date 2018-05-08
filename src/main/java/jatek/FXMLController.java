69a54e85ee07a36f5843702bf77b1c80660ddb51
fykamie <esztike@acsgabi.com>
2018-05-08 13:42:19 +0200

    frissites

--- src/main/java/jatek/FXMLController.java
+++ src/main/java/jatek/FXMLController.java
@@ -5,21 +5,35 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Screen;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
+    private ImageView batfarao= new ImageView();
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
-        label.setText("Hello World!");
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    public void afterInitialize(){
+        imageViewModifier();
+    }
+    
+    public void imageViewModifier(){
+        javafx.geometry.Rectangle2D screenBounds= Screen.getPrimary().getVisualBounds();
+        
+        batfarao.setFitHeight(screenBounds.getHeight());
+        batfarao.setFitWidth(screenBounds.getWidth()/6);
+        batfarao.setLayoutX( screenBounds.getMinX() + screenBounds.getWidth() - batfarao.getFitWidth() -7);
+//        batfarao.setStyle("-fx-background-color: red");
+    }
 }
