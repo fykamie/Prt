f471e2a2431f6a4efd3ace32adaab8f452dcc4c7
fykamie <esztike@acsgabi.com>
2018-05-08 18:37:28 +0200

    gombok labelek hozzaadasa

--- src/main/java/jatek/FXMLController.java
+++ src/main/java/jatek/FXMLController.java
@@ -1,39 +1,158 @@
 package jatek;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 
+/**
+ * FXML Controller class
+ *
+ * @author eszti
+ */
 public class FXMLController implements Initializable {
     
     @FXML
-    private ImageView batfarao= new ImageView();
-    
+    private Pane pane;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-    }
-    
+    private Button randomCube;
+    @FXML
+    private Button selectedCube;
+    @FXML
+    private Label ownPyramidTop;
+    @FXML
+    private Label sellerPyramidTop;
+    @FXML
+    private Label littlePyramidTop;
+    @FXML
+    private ImageView batfarao;
+
+    private final javafx.geometry.Rectangle2D primaryScreenBounds= Screen.getPrimary().getVisualBounds();
+    private final Double screenWidth= primaryScreenBounds.getWidth();
+    private final Double screenHeight= primaryScreenBounds.getHeight();
+
+    private ArrayList<Button> ownCubesList;
+
+    private ArrayList<Label> sellerCubesList;
+    private Kockak cube= new Kockak();
+    private ReadyForStart readyForStart= new ReadyForStart();
+    private final int myWidther= 6;
+    private final int myLayoutX= 170;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+    public void initialize(URL url, ResourceBundle rb) {}
 
     public void afterInitialize(){
-        imageViewModifier();
+
+        batfarao.setFitHeight(screenHeight);
+        batfarao.setFitWidth(screenWidth/6);
+        batfarao.setLayoutX(primaryScreenBounds.getMinX()+screenWidth-batfarao.getFitWidth()-7);
+        
+        cube= readyForStart.makeReady(cube);
+        
+        ownCubesList= new ArrayList<>();
+        sellerCubesList= new ArrayList<>();
+        randomCube= new Button("random");
+        selectedCube= new Button("selected");
+        ownPyramidTop= new Label();
+        sellerPyramidTop= new Label();
+        littlePyramidTop= new Label();
+        
+        
+        for(int i= 0; i<20; i++ ){
+            Button but= new Button("ownCube"+i);
+            Label lab= new Label("sellerCube"+i);
+            
+            but.setLayoutY(60);
+            lab.setLayoutY(60);
+            
+            ownCubesList.add(but);
+            sellerCubesList.add(lab);
+            pane.getChildren().add(lab);
+            pane.getChildren().add(but);
+            
+        }
+        
+        for(int i= 19; i>=0; i--){
+            
+            this.ownCubesList.get(i).setPrefHeight(30);
+            this.sellerCubesList.get(i).setPrefHeight(30);
+            this.ownCubesList.get(i).setLayoutY(this.ownCubesList.get(i).getLayoutY()+(i+1)*25);
+            this.sellerCubesList.get(i).setLayoutY(this.sellerCubesList.get(i).getLayoutY()+(i+1)*25);
+        }
+
+        pane.getChildren().add(randomCube);
+        pane.getChildren().add(selectedCube);
+        pane.getChildren().add(ownPyramidTop);
+        pane.getChildren().add(sellerPyramidTop);
+        pane.getChildren().add(littlePyramidTop);
+
+        modifyOwnCubesList();
+        modifySellerCubesList();
+        modifyRandomCube();
+        modifySelectedCube();
+
+//        randomCube.setOnAction( (ev) -> clickedRandomCube(ev) );
+//        selectedCube.setOnAction( (ev) -> clickedSelectedCube(ev) );
+       
+        
     }
     
-    public void imageViewModifier(){
-        javafx.geometry.Rectangle2D screenBounds= Screen.getPrimary().getVisualBounds();
+    public void modifyOwnCubesList(){
+        for(int i= 19; i>=0; i--){
+            if ( this.cube.getSajatKockaimLista().get(i) < 5) this.ownCubesList.get(i).setPrefWidth(30);
+            else this.ownCubesList.get(i).setPrefWidth(this.cube.getSajatKockaimLista().get(i)*myWidther);
+            this.ownCubesList.get(i).setLayoutX(myLayoutX-this.ownCubesList.get(i).getPrefWidth()/2);
+            this.ownCubesList.get(i).setText(this.cube.getSajatKockaimLista().get(i).toString());
+            this.ownCubesList.get(i).setDisable(true);
+        }
         
-        batfarao.setFitHeight(screenBounds.getHeight());
-        batfarao.setFitWidth(screenBounds.getWidth()/6);
-        batfarao.setLayoutX( screenBounds.getMinX() + screenBounds.getWidth() - batfarao.getFitWidth() -7);
-//        batfarao.setStyle("-fx-background-color: red");
     }
+   
+    public void modifySellerCubesList(){
+        for(int i= 0; i<20; i++){
+            if ( this.cube.getEllenfelKockaiLista().get(i) < 5) this.sellerCubesList.get(i).setPrefWidth(30);
+            else this.sellerCubesList.get(i).setPrefWidth(this.cube.getEllenfelKockaiLista().get(i)*myWidther);
+            this.sellerCubesList.get(i).setLayoutX(myLayoutX+650-this.sellerCubesList.get(i).getPrefWidth()/2);
+            this.sellerCubesList.get(i).setText(this.cube.getEllenfelKockaiLista().get(i).toString());
+//            this.sellerCubesList.get(i).setTextAlignment(TextAlignment.CENTER);
+            this.sellerCubesList.get(i).setEffect(new GaussianBlur());
+        }
+    }
+    
+    public void modifyRandomCube(){      
+        if ( this.cube.getCsereleshezKocka()< 5) this.randomCube.setPrefWidth(30);
+            else this.randomCube.setPrefWidth(this.selectedCube.getPrefWidth()*2/3);
+            this.randomCube.setLayoutX(myLayoutX+325-this.randomCube.getPrefWidth()/2);
+            this.randomCube.setText( "?" );
+            this.randomCube.setLayoutY(535);
+            this.randomCube.setDisable(false);
+    }
+    
+    public void modifySelectedCube(){
+        if ( this.cube.getCsereleshezKocka()< 5) this.selectedCube.setPrefWidth(30);
+            else this.selectedCube.setPrefWidth(this.cube.getCsereleshezKocka()*myWidther);
+            this.selectedCube.setLayoutX(myLayoutX+325-this.selectedCube.getPrefWidth()/2);
+            this.selectedCube.setText( this.cube.getCsereleshezKocka().toString());
+            this.selectedCube.setLayoutY(560);
+            this.selectedCube.setDisable(false);
+    }
+    
+    
 }
