0c65fe77892c28d2b0ddb551c79763827b15c25f
fykamie <esztike@acsgabi.com>
2018-05-11 17:18:00 +0200

    tesztek

--- src/test/java/jatek/KockakTest.java
+++ src/test/java/jatek/KockakTest.java
@@ -111,159 +111,35 @@ public class KockakTest {
         assertEquals(5, kockak.getPontok().getMySource());
     }
 
-//    @Test
-//    public void testSwapCserelniEsRandomKockak() {
-//        System.out.println("swapCserelniEsRandomKockak");
-//        Integer hol = null;
-//        Kockak instance = new Kockak();
-//        instance.swapCserelniEsRandomKockak(hol);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of swapEgyRandomEsSajatKockaim method, of class Kockak.
-//     */
-//    @Test
-//    public void testSwapEgyRandomEsSajatKockaim() {
-//        System.out.println("swapEgyRandomEsSajatKockaim");
-//        Integer hol = null;
-//        Kockak instance = new Kockak();
-//        instance.swapEgyRandomEsSajatKockaim(hol);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of swapRandomKockaEsCsereleshez method, of class Kockak.
-//     */
-//    @Test
-//    public void testSwapRandomKockaEsCsereleshez() {
-//        System.out.println("swapRandomKockaEsCsereleshez");
-//        Kockak instance = new Kockak();
-//        instance.swapRandomKockaEsCsereleshez();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of megvizsgalomSorbanVannakSajatKockaim method, of class Kockak.
-//     */
-//    @Test
-//    public void testMegvizsgalomSorbanVannakSajatKockaim() {
-//        System.out.println("megvizsgalomSorbanVannakSajatKockaim");
-//        Kockak instance = new Kockak();
-//        boolean expResult = false;
-//        boolean result = instance.megvizsgalomSorbanVannakSajatKockaim();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of megvizsgalomSorbanVannakEllenfelKockai method, of class Kockak.
-//     */
-//    @Test
-//    public void testMegvizsgalomSorbanVannakEllenfelKockai() {
-//        System.out.println("megvizsgalomSorbanVannakEllenfelKockai");
-//        Kockak instance = new Kockak();
-//        boolean expResult = false;
-//        boolean result = instance.megvizsgalomSorbanVannakEllenfelKockai();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of randomotDob method, of class Kockak.
-//     */
-//    @Test
-//    public void testRandomotDob() {
-//        System.out.println("randomotDob");
-//        Kockak instance = new Kockak();
-//        Integer expResult = null;
-//        Integer result = instance.randomotDob();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of isAdatbazisEmpty method, of class Kockak.
-//     */
-//    @Test
-//    public void testIsAdatbazisEmpty() {
-//        System.out.println("isAdatbazisEmpty");
-//        Kockak instance = new Kockak();
-//        boolean expResult = false;
-//        boolean result = instance.isAdatbazisEmpty();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of adatbazisbaMent method, of class Kockak.
-//     */
-//    @Test
-//    public void testAdatbazisbaMent() {
-//        System.out.println("adatbazisbaMent");
-//        Kockak instance = new Kockak();
-//        instance.adatbazisbaMent();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of datbazisbolKiszedi method, of class Kockak.
-//     */
-//    @Test
-//    public void testDatbazisbolKiszedi() {
-//        System.out.println("datbazisbolKiszedi");
-//        Kockak instance = new Kockak();
-//        Kockak expResult = null;
-//        Kockak result = instance.datbazisbolKiszedi();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of adatbazisRekordokTorlese method, of class Kockak.
-//     */
-//    @Test
-//    public void testAdatbazisRekordokTorlese() {
-//        System.out.println("adatbazisRekordokTorlese");
-//        Kockak instance = new Kockak();
-//        instance.adatbazisRekordokTorlese();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of closeConnect method, of class Kockak.
-//     */
-//    @Test
-//    public void testCloseConnect() {
-//        System.out.println("closeConnect");
-//        Kockak instance = new Kockak();
-//        instance.closeConnect();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of toString method, of class Kockak.
-//     */
-//    @Test
-//    public void testToString() {
-//        System.out.println("toString");
-//        Kockak instance = new Kockak();
-//        String expResult = "";
-//        String result = instance.toString();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testSwapCserelniEsRandomKockak() {
+        kockak.setCsereleshezKocka(6);
+        kockak.swapCserelniEsRandomKockak(0);
+        assertEquals(Arrays.asList(6, 2, 3), kockak.getRandomKockakLista());
+    }
+
+    @Test
+    public void testSwapEgyRandomEsSajatKockaim() {
+        kockak.swapEgyRandomEsSajatKockaim(0);
+        assertEquals(Arrays.asList(5, 2, 3), kockak.getSajatKockaimLista());
+        assertTrue(kockak.getCsereleshezKocka().equals(1));
+    }
+
     
+    @Test
+    public void testSwapRandomKockaEsCsereleshez() {
+        kockak.swapRandomKockaEsCsereleshez();
+        assertTrue(kockak.getCsereleshezKocka().equals(5));
+        assertTrue(kockak.getRandomKocka().equals(6));
+    }
+
+    @Test
+    public void testMegvizsgalomSorbanVannakSajatKockaim() {
+        assertTrue(kockak.megvizsgalomSorbanVannakSajatKockaim());
+    }
+
+    @Test
+    public void testMegvizsgalomSorbanVannakEllenfelKockai() {
+        assertTrue(kockak.megvizsgalomSorbanVannakEllenfelKockai());
+    }    
 }
